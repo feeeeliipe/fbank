752d28acb1d2ded89c9cd9dc04ab21aef33d6ffd
feeeeliipe <felipe.rinaldi.goncalves@gmail.com>
2021-07-09 23:54:10 -0300

    feat: renaming account data queue and creating update customer credit queue

--- services/credit-analyzer-service/src/events/UpdateCustomerCreditHandler.ts
+++ services/credit-analyzer-service/src/events/UpdateCustomerCreditHandler.ts
@@ -1,9 +1,29 @@
+import { Logger } from "../../../../lib/Logger";
+import { SQSEventParser } from "../../../../lib/SQSEventParser";
+
 export class UpdateCustomerCreditHandler {
 
-    async handle(event: any) {
-        // TODO: Recalculate customer credit and save
-    }
+    private logger = new Logger('UpdateCustomerCreditHandler');
 
+    async handler(event: any) {
+        try {
+            const messages = SQSEventParser.parse(event);
+            this.logger.info('Update customer credit requested', {
+                eventName: 'UpdateCustomerCreditRequested',
+                messages
+            });
+            // TODO: Recalculate customer credit and save into service database
+        } catch (error) {
+            this.logger.error('Error recalculating customer credit', {
+                eventName: 'UpdateCustomerCreditHandlerError',
+                message: error.message,
+                stack: error.stack
+            });
+        }
+    }
 }
 
-
+const updateCustomerCreditHandler = new UpdateCustomerCreditHandler();
+module.exports = {
+    handler: updateCustomerCreditHandler.handler.bind(updateCustomerCreditHandler)
+}
